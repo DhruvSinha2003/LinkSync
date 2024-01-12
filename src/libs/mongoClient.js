a61c6cd3c8a0c6e7950dd0a2ae1552779a49e407
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-01-12 23:54:36 +0530

    this time pakka hogaya

--- src/libs/mongoClient.js
+++ src/libs/mongoClient.js
@@ -1,25 +1,25 @@
-// import { MongoClient } from "mongodb"
+import { MongoClient } from "mongodb"
 
-// if (!process.env.MONGO_URI) {
-//   throw new Error('Invalid/Missing environment variable: "MONGO_URI"');
-// }
+if (!process.env.MONGO_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGO_URI"');
+}
 
-// const uri = process.env.MONGO_URI;
+const uri = process.env.MONGO_URI;
 
-// const options = {}
+const options = {}
 
-// let client
-// let clientPromise
+let client
+let clientPromise
 
-// if (process.env.NODE_ENV === "development") {
-//   if (!global._mongoClientPromise) {
-//     client = new MongoClient(uri, options)
-//     global._mongoClientPromise = client.connect()
-//   }
-//   clientPromise = global._mongoClientPromise
-// } else {
-//   client = new MongoClient(uri, options)
-//   clientPromise = client.connect()
-// }
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options)
+    global._mongoClientPromise = client.connect()
+  }
+  clientPromise = global._mongoClientPromise
+} else {
+  client = new MongoClient(uri, options)
+  clientPromise = client.connect()
+}
 
-// export default clientPromise;
+export default clientPromise;
